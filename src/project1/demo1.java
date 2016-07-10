bc8a1d45166806d035b4762e152a7f29be6e9bbe
Varun Kumar <varun@g_mail.com>
2016-07-10 14:56:49 +0530

    New comment

--- src/project1/demo1.java
+++ src/project1/demo1.java
@@ -3,7 +3,7 @@ package project1;
 public class demo1 {
 	public static void main(String[] args) {
 
-		System.out.println("hello"); // Soem comment
+		System.out.println("hello"); // Some comment
 	}
 
 }
