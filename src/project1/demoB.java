a3e45970ec454a0e4576f7e4cb49cd8a76768db7
laalchee <varun.forums@gmail.com>
2016-07-10 15:01:07 +0530

    Class B comment

--- src/project1/demoB.java
+++ src/project1/demoB.java
@@ -4,6 +4,6 @@ package project1;
 public class demoB {
 
     public static void main(String[] args) {
-        System.out.println("new class");
+        System.out.println("new class"); // new comment
     }
 }
